fabac60417dfcbd0eb1a7a31b0eddb91ec3881f8
chitralakshmi-ops <chitrathangaraj2007@gmail.com>
2026-09-17 13:15:30 +0530

    Time: 16 ms (19.07%) | Memory: 47.2 MB (12.68%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer>map1=new HashMap<>();
+        HashMap<Character,Integer>map2=new HashMap<>();
+        for(int i=0;i<ransomNote.length();i++){
+           map1.put(ransomNote.charAt(i),map1.getOrDefault(ransomNote.charAt(i),0)+1); 
+        }
+        for(int i=0;i<magazine.length();i++){
+            map2.put(magazine.charAt(i),map2.getOrDefault(magazine.charAt(i),0)+1);
+        }
+        for(char ch:map1.keySet()){
+            if(!map2.containsKey(ch)){
+                return false;
+            }
+            if(map1.get(ch)>map2.get(ch)){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
