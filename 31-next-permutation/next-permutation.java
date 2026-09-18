34c5afa0fd7e22ad950ab08d140c436eeeb4eb80
chitralakshmi-ops <chitrathangaraj2007@gmail.com>
2026-09-18 11:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (51.47%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i=nums.length-2;
+        //find pivot value from right
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+        //find greater number than the pivot value from right & swap it with the pivot value
+        if(i>=0){
+            int j=nums.length-1;
+            while(nums[j]<=nums[i]){
+                j--;
+            }
+            int t=nums[i];
+            nums[i]=nums[j];
+            nums[j]=t;
+        }
+        //reverse all the elements immediate next to the swapped new number
+        int left=i+1;
+        int right=nums.length-1;
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
